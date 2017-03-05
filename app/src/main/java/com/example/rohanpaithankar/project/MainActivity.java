b88ef58e123c9eb18ee4459de7cec72abea4281f
rohanpaithankar <paithankarrohan039@gmail.com>
2017-03-05 13:02:26 +0530

    vector

--- app/src/main/java/com/example/rohanpaithankar/project/MainActivity.java
+++ app/src/main/java/com/example/rohanpaithankar/project/MainActivity.java
@@ -1,15 +1,26 @@
 package com.example.rohanpaithankar.project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    Button b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("This is the change");
+        b=(Button)findViewById(R.id.Search);
+        b.setOnClickListener(this);
+    }
+    public void onClick(View v){
+        if(v.getId()==R.id.Search){
+          Intent i=new Intent(getApplicationContext(),search_activity.class);
+            startActivity(i);
 
+        }
     }
 }
---
+++ app/src/main/java/com/example/rohanpaithankar/project/search_activity.java
@@ -0,0 +1,15 @@
+package com.example.rohanpaithankar.project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class search_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_activity);
+        
+    }
+
+}
