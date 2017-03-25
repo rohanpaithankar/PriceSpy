a860100a48163cd5f3bfd26bdb42d356f852aa85
rohanpaithankar <paithankarrohan039@gmail.com>
2017-03-25 17:56:48 +0530

    -

--- app/src/main/java/com/example/rohanpaithankar/project/search_activity.java
+++ app/src/main/java/com/example/rohanpaithankar/project/search_activity.java
@@ -9,7 +9,7 @@ public class search_activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_activity);
-        
+
     }
 
 }
