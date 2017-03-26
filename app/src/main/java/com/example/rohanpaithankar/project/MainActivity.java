3786d2adf7d306dbcee4f6c8cfac3760c985a1cd
rohanpaithankar <paithankarrohan039@gmail.com>
2017-03-26 16:53:27 +0530

    update

--- app/src/main/java/com/example/rohanpaithankar/project/MainActivity.java
+++ app/src/main/java/com/example/rohanpaithankar/project/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.rohanpaithankar.project;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 
@@ -15,6 +17,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         b=(Button)findViewById(R.id.Search);
         b.setOnClickListener(this);
+    }
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.activity_main_menu,menu);
+        return true;
+
     }
     public void onClick(View v){
         if(v.getId()==R.id.Search){
