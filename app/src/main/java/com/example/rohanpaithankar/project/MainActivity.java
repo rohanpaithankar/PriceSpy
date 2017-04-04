4008de8ef7924e678e8607c4e43d76c807c257fe
rohanpaithankar <paithankarrohan039@gmail.com>
2017-04-04 19:14:51 +0530

    fade-in

--- app/src/main/java/com/example/rohanpaithankar/project/MainActivity.java
+++ app/src/main/java/com/example/rohanpaithankar/project/MainActivity.java
@@ -7,16 +7,28 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    Button b;
+    Button b1,b2;
+    TextView t;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        b=(Button)findViewById(R.id.Search);
-        b.setOnClickListener(this);
+        b1=(Button)findViewById(R.id.Search);
+        b2=(Button)findViewById(R.id.category);
+        t=(TextView)findViewById(R.id.tap);
+        b1.setOnClickListener(this);
+        Animation animation= AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade_in);
+        b1.startAnimation(animation);
+        b2.startAnimation(animation);
+        t.startAnimation(animation);
+
+
     }
     public boolean onCreateOptionsMenu(Menu menu)
     {
